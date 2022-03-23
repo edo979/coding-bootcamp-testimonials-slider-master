826b84049047b25fb8ff2fe594d021e20ee35e6a
edo979 <prezentabilan@gmail.com>
2022-03-23 23:24:20 +0100

    js slider control finish

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const prevBtn = document.getElementById('prev'),
-  nextBtn = document.getElementById('next')
+  nextBtn = document.getElementById('next'),
+  slidesEl = document.querySelector('.slider-slides'),
+  numOfSlides = document.querySelectorAll('.slider-slide').length
+
+let currentSlide = 0
 
 // Event listeners
-prevBtn.addEventListener('click', (e) => {})
-nextBtn.addEventListener('click', (e) => {})
+prevBtn.addEventListener('click', (e) => {
+  currentSlide--
+  changeSlide()
+})
+nextBtn.addEventListener('click', (e) => {
+  currentSlide++
+  changeSlide()
+})
+
+function changeSlide() {
+  if (currentSlide < 0) {
+    currentSlide = numOfSlides - 1
+  }
+  if (currentSlide >= numOfSlides) {
+    currentSlide = 0
+  }
+
+  slidesEl.style.left = `${-currentSlide * 100}%`
+  console.log(currentSlide)
+}
