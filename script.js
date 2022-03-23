73ac075faaeea5b3d4518c103150bc827f90b77d
edo979 <prezentabilan@gmail.com>
2022-03-23 21:11:08 +0100

    add js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const prevBtn = document.getElementById('prev'),
+  nextBtn = document.getElementById('next')
+
+// Event listeners
+prevBtn.addEventListener('click', (e) => {})
+nextBtn.addEventListener('click', (e) => {})
